01455dd0032b9830b49578552f5e28b4a4c22d56
MoNoLidThZ <admin@monolidthz.com>
2015-07-03 18:58:22 +0700

    Version 2.4
    + Added API Version 1.0
    * Changed SPKZ_DKS_Deathmatch convar to SPKZ_DKS_TeamMode

--- lua/autorun/server/spkz_dks.lua
+++ lua/autorun/server/spkz_dks.lua
@@ -12,22 +12,24 @@
 --	$!nG1_ePlAyErZ's Dota Kill Streak (yep I know the sound is from UT2K4)
 --		CREDITS:
 --			Team Ulysses						-	ULX CSay code that I've hijacked it
---			Bronisyu	(STEAM_0:1:35342659)	-	Bug reporting and testing
 -- Why the hell are you decompiling my code?
 -- Version 2.3 Notes:
---	Tested on: Sandbox, Prop Hunt (nothing else but you can test and bug report to me)
+-- 	+ Added API Version 1.0
+-- 	* Changed SPKZ_DKS_Deathmatch command to SPKZ_DKS_TeamMode 
+--	Tested on: Sandbox (nothing else but you can test and bug report to me)
 -- If you're reading this, you have my permission to edit, modify, remake the code but you will required to leave me as a credit.
 -- Also, you should consider about buy me a beer
 --	PayPal:		admin (at) monolidthz.com
 --	Dogecoin:	DSUQoCu2fHT1LQTJTF5wSUZ6C8u3TvQs5i
-	local version = "2.3"
-	local releasedate = "28/10/2014"
+	local version = "2.4"
+	local releasedate = "03/07/2015"
 	local TotalKills = 0
+DotaKillStreak = {}
 local function InitSPKZCmds()
 		MsgC( Color( 0, 255, 0 ), "$!nG1_ePlAyErZ's Dota Kill Streak v".. version .."("..releasedate..") Initialized\n" )
 		--end
 		CreateConVar( "SPKZ_DKS_Enabled", "1", FCVAR_ARCHIVE, "If 0, Disable the entire system" )
-		CreateConVar( "SPKZ_DKS_Deathmatch", "1", FCVAR_ARCHIVE, "If 1, Ignore Friendly Fire Checking" )
+		CreateConVar( "SPKZ_DKS_TeamMode", "0", FCVAR_ARCHIVE, "If 0, Ignore Friendly Fire Checking" )
 		CreateConVar( "SPKZ_DKS_NPCs", "1", FCVAR_ARCHIVE, "If 1, NPC Kills will also trigger the killstreak" )
 		CreateConVar( "SPKZ_DKS_CSKillTimer", "15", FCVAR_ARCHIVE, "How Many Seconds that Consecutive Kill Timer Reset?" )
 		CreateConVar( "SPKZ_DKS_Broadcast", "1", FCVAR_ARCHIVE, "Only send message to every player or only killer?" )
@@ -59,7 +61,7 @@ local function InitSPKZCmds()
 	hook.Add("Initialize", "SPKZ_Init", InitSPKZCmds)
 	local killstreakNames = { "","","Killing Spree","Dominating","Mega Kill","Unstoppable","Wicked Sick","Monster Kill","GODLIKE","Beyond GODLIKE" }
 local function CheckFF(ply1,ply2) --Return TRUE if is friendly fire
-	if not cvars.Bool("spkz_dks_deathmatch") then return false end
+	if not cvars.Bool("SPKZ_DKS_TeamMode") then return false end
 	if(not ply1:IsPlayer() and not ply2:IsPlayer()) then return end
 	local Team1 = ply1:Team()
 	local Team2 = ply2:Team()
@@ -67,7 +69,6 @@ local function CheckFF(ply1,ply2) --Return TRUE if is friendly fire
 	end
 	--PLY
 local function SPKZ_PlayerKilled( ply, inflictor, attacker )
-	-- debug.Trace()
 	if not cvars.Bool("spkz_dks_enabled") then return end
 	if !IsValid(attacker) then return end
 	if(inflictor:GetClass() == "worldspawn")then	
@@ -103,21 +104,19 @@ local function SPKZ_NPCKilled( npc, attacker,inflictor )
      end
 	end
 hook.Add("OnNPCKilled","SPKZ_NPCKilled",SPKZ_NPCKilled)
-	--Workaround for Prop Hunt Gamemode
-function SPKZ_EntityTakeDamage(ent, dmginfo)
+-- Workaround for Prop Hunt Gamemode
+local function SPKZ_EntityTakeDamage(ent, dmginfo)
     local att = dmginfo:GetAttacker()
 	if ent:GetClass() != "ph_prop" then return end
 	ent.SPKZ_LastDamaged = att
 end
 hook.Add("EntityTakeDamage", "SPKZ_EntityTakeDamage", SPKZ_EntityTakeDamage)
-function SPKZ_PlayerSilentDeath(ply)
+local function SPKZ_PlayerSilentDeath(ply)
 	if !ply.ph_prop then return end
 	if !ply.ph_prop.SPKZ_LastDamaged then return end
 	SPKZ_IncKill(ply.ph_prop.SPKZ_LastDamaged,ply)
 end
 hook.Add("PlayerSilentDeath", "SPKZ_PlayerSilentDeath", SPKZ_PlayerSilentDeath)
-	--Assists SOON!
-	
 	--Reset All code (for fretta based gamemode and uhh... TTT)
 function SPKZ_ResetAll()
 	MsgC( Color( 0, 128, 255 ), "[SPKZDKS]: Resetting all Killstreak Data...\n" )
@@ -128,7 +127,8 @@ function SPKZ_ResetAll()
 	TotalKills = 0
 end
 hook.Add("TTTEndRound","SPKZ_ResetAll",SPKZ_ResetAll)
-function SPKZ_PostGamemodeLoaded()
+-- override base function lel
+local function SPKZ_PostGamemodeLoaded()
 	if GAMEMODE.OnRoundEnd or GAMEMODE.BaseClass.OnRoundEnd then
 		local oldfn = GAMEMODE.OnRoundEnd or GAMEMODE.BaseClass.OnRoundEnd
 		function GAMEMODE:OnRoundEnd(...)
@@ -173,4 +173,26 @@ function SPKZ_IncCSKill(killer)
 		killer:SetNWInt("SPKZ_CSKillStreak",1)
 	end)
 	return n
+end
+
+-- API Code starts here
+DotaKillStreak.PlayerDeath = function( victim, inflictor, attacker )
+	SPKZ_PlayerKilled(victim, inflictor, attacker)
+end
+
+DotaKillStreak.ResetAll = function()
+	SPKZ_ResetAll()
+end
+
+DotaKillStreak.GetKill = function(ply)
+	if not IsValid(ply) then return end
+	return ply:GetNWInt("SPKZ_CSKillStreak",1)
+end
+
+DotaKillStreak.SetKill = function(ply,newvalue)
+	if not IsValid(ply) then return end
+	if not isnumber(newvalue) then
+		newvalue = tonumber(newvalue)
+	end
+	ply:SetNWInt("SPKZ_CSKillStreak",newvalue)
 end
